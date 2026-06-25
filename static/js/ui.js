273751bdeaf588653f2b9081b947db8bd1cb1d2a
NuhaMana <nuhamanajirr@gmail.com>
2026-06-25 19:14:26 +0530

    refined navbar

--- static/js/ui.js
+++ static/js/ui.js
@@ -1,1 +1,13 @@
 console.log("Smart Waste UI loaded");
+
+const currentPath = window.location.pathname;
+
+document.querySelectorAll(".nav-link-btn").forEach(link => {
+
+    if (link.getAttribute("href") === currentPath) {
+
+        link.classList.add("active");
+
+    }
+
+});
